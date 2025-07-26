a28e1e0d406978aad7b7e1c5172853377a21d4d2
Amin <amin.zahedi.2002@gmail.com>
2025-07-26 16:20:01 +0330

    Create MailAutomation.Domain

---
+++ MailAutomation.Domain/Mail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailAutomation.Domain
+{
+    public class Mail
+    {
+        public string MailId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public bool IsRemovedFromSender { get; set; }
+        public bool IsRemovedFromReceiver { get; set; }
+
+        public User Sender { get; set; }
+        public User Receiver { get; set; }
+    }
+}
