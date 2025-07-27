a933a9b1ec01cff056786b11c6f8b31eb12a2b9d
Amin <amin.zahedi.2002@gmail.com>
2025-07-27 13:13:57 +0330

    Working on Infrustructure -> User services

---
+++ MailAutomation.Infrastructure/Services/UserServices/SignUpUser.cs
@@ -0,0 +1,42 @@
+﻿using MailAutomation.Application.Common;
+using MailAutomation.Application.UserServices.Commands;
+using MailAutomation.Domain;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailAutomation.Infrastructure.Services.UserServices
+{
+    public class SignUpUser : ISignUpUser
+    {
+        private readonly UserManager<User> _userManager;
+
+        public SignUpUser(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public ResultDto SignUp(UserDto user)
+        {
+            User userToSignUp = new User()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName
+            };
+
+            var result = _userManager.CreateAsync(userToSignUp, user.Passwrod).Result;
+
+            if (result.Succeeded)
+                return new ResultDto(true, "s_100");
+            else
+                return new ResultDto(false, "f_110", result.Errors.Select(e => e.Description).ToList());
+
+
+        }
+    }
+}
