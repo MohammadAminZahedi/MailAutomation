1cfd3042953d7958af64fdec036f61b7a704e9a5
Amin <amin.zahedi.2002@gmail.com>
2025-07-31 18:29:38 +0330

    Update ResultDto.cs
    Results enum added.

--- MailAutomation.Application/Common/ResultDto.cs
+++ MailAutomation.Application/Common/ResultDto.cs
@@ -10,17 +10,32 @@ namespace MailAutomation.Application.Common
     {
         public bool State { get; set; }
         public string Code { get; set; }
+        public Results Comment { get; set; }
         public List<string> Errors { get; set; }
 
-        public ResultDto(bool state, string code)
+        public ResultDto(bool state, Results comment)
         {
             State = state;
-            Code = code;
+            Comment = comment;
         }
 
-        public ResultDto(bool state, string code, List<string> errors) : this(state, code)
-        {
-            Errors = errors;
-        }
+       
+
+       
     }
+    public enum Results
+    {
+        Success,
+        Failure,
+        DatabaseError,
+        ApplicationError,
+        UserAleardyExist,
+        UserNotFound,
+        SginInError,
+        SignUpError,
+
+        MailNotFound
+    }
+
+  
 }
