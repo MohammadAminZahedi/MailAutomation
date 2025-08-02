b4535e97b00c565a9aafab4e39504db1782212bb
Amin <amin.zahedi.2002@gmail.com>
2025-08-02 12:47:17 +0330

    Implement Sign in

--- MailAutomation.Presentation/Controllers/AccountingController.cs
+++ MailAutomation.Presentation/Controllers/AccountingController.cs
@@ -1,5 +1,6 @@
 ﻿using MailAutomation.Application.Common;
 using MailAutomation.Application.UserServices.Commands;
+using MailAutomation.Application.UserServices.Queries;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MailAutomation.Presentation.Controllers
@@ -7,10 +8,12 @@ namespace MailAutomation.Presentation.Controllers
     public class AccountingController : Controller
     {
         private readonly ISignUpUser _signUpUser;
+        private readonly ISignInUser _signInUser;
 
-        public AccountingController(ISignUpUser signUpUser)
+        public AccountingController(ISignUpUser signUpUser, ISignInUser signInUser)
         {
             _signUpUser = signUpUser;
+            _signInUser = signInUser;
         }
 
         public IActionResult SignUp()
@@ -30,6 +33,22 @@ namespace MailAutomation.Presentation.Controllers
         public IActionResult SignIn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(UserDto userDto)
+        {
+            var result = _signInUser.SignIn(userDto);
+            if (result.State)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ResultState = result.State;
+                return View();
+            }
+
         }
 
     }
